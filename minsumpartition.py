9b6bc8995cc335ba70c78aa3fe7d2fe40c18a97d
Derrick Knighton <Derrickknighton22@Gmail.com>
2019-11-19 17:24:00 -0500

    Update minsumpartition.py
    edited it so it actually works now was doing a few things in this that broke it before hand such as using indeces as the max instead of the vals (rookie mistake)

--- minsumpartition.py
+++ minsumpartition.py
@@ -24,43 +24,52 @@ class Min_Sum_Partition():
     
     def Partition(self,arr):
         #split array into 2 sub arrays such that the summation of he 2 is minimized
-        #Algo idea find max in array put in one of the subarrays
-        #then remove max find new max and put that in other subarray
+        #Algo idea find max in the input array add it to ont of the arrays such that
+        #the difference of the summation of the arrays is smaller than if you added it to the other
+        #if the difference of them is the same add to array 2 it doesnt really mater what one you add it to
         #continue until arr is size 0
-        
         sub_arr_one = np.ndarray([0])
         sub_arr_two = np.ndarray([0])
         
         
         #this algo works with only posiive numbers to init max to negative
-        max = -1000
+        max = np.argmax(arr)
         num_to_add = -1000
+        sum1 = 0
+        sum2 = 0
         
-        #bool to flip flop between sub_arr_one and two
-        add_to_arr = True
-        
-        while max != 0:
-            
-            max = np.argmax(arr)
-            
+        while arr[max] != 0:
+            print(arr)
+            print(arr[max])
             #####################################################
             #if statements take max in arr add to one sub_arr delete the max element in
             #arr and change the bool so next max is added to other sub arr
             #let it be known it is completely unesscessary to add these vals to arrays
             #this could be accomplished more efficiently by just summing the values
             #immediately and that would take up less space i just wanted to
-            #do it his way
-            if add_to_arr == True:
+            #do it his way to show the arrays so you can see where the values ended up
+            
+            sum1_Try = sum1 + int(arr[max])
+            sum2_Try = sum2 + int(arr[max])
+            
+            if abs(sum1_Try - sum2) < abs(sum2_Try - sum1):
                 num_to_add = arr[max] 
                 sub_arr_one = np.append(sub_arr_one,[num_to_add])
-                add_to_arr = False
+                sum1 += arr[max]
                 arr[max] = 0
                 
-            elif add_to_arr == False:
+            elif abs(sum1_Try - sum2) > abs(sum2_Try - sum1):
                 num_to_add = arr[max]
                 sub_arr_two = np.append(sub_arr_two,[num_to_add])
-                add_to_arr = True
+                sum2 += arr[max]
                 arr[max] = 0
+                
+            elif abs(sum1_Try - sum2) == abs(sum2_Try - sum1):
+                num_to_add = arr[max]
+                sub_arr_two = np.append(sub_arr_two,[num_to_add])
+                sum2 += arr[max]
+                arr[max] = 0
+            max = np.argmax(arr)
             #####################################################
         return sub_arr_one, sub_arr_two   
     
@@ -70,7 +79,7 @@ class Min_Sum_Partition():
         #sum sub arrs and take difference
         sum1 = np.sum(sub_arr_one)
         sum2 = np.sum(sub_arr_two)
-        
+        print(sum1,sum2)
         
         final_Answer = np.abs(sum1 - sum2)
         
@@ -80,10 +89,10 @@ class Min_Sum_Partition():
 
 if __name__ == "__main__":
     run = Min_Sum_Partition()
-    arr = run.create_Random_Array(100,100)
+    arr = run.create_Random_Array(1000,100)
+    arr2 = [1,1,3,4,7]
     sub_arr_one, sub_arr_two = run.Partition(arr)
     print(run.Min_Sum(sub_arr_one,sub_arr_two))
-    
     
     
     
